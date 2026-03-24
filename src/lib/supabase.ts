0a686be92f62770e778ec64caf4d4ef6a89c6a83
Malkiasystems <jawabu92@gmail.com>
2026-03-24 18:08:08 +0300

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,12 +1,17 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient(
-  'https://ebokhvibnypiomzqimfg.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVib2todmlibnlwaW9tenFpbWZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQwMzA3MDIsImV4cCI6MjA4OTYwNjcwMn0.yqaB42lvEN_vkUt1q6VBAAHdwSYOaIwt8bH5Vg9MTQk',
-  {
-    auth: {
-      persistSession: false,
-      autoRefreshToken: false,
+const URL = 'https://ebokhvibnypiomzqimfg.supabase.co'
+const KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVib2todmlibnlwaW9tenFpbWZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQwMzA3MDIsImV4cCI6MjA4OTYwNjcwMn0.yqaB42lvEN_vkUt1q6VBAAHdwSYOaIwt8bH5Vg9MTQk'
+
+export const supabase = createClient(URL, KEY, {
+  global: {
+    headers: {
+      apikey: KEY,
+      Authorization: `Bearer ${KEY}`,
     }
+  },
+  auth: {
+    persistSession: false,
+    autoRefreshToken: false,
   }
-)
+})
